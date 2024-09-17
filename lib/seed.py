89ae37347e31a60515a7eeaecc893d38bf55ba4f
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-09-17 23:00:03 +0300

    seeds the questions database

--- lib/seed.py
+++ lib/seed.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-from helpers import create_job_field,delete_job_field
+from helpers import create_job_field,delete_job_field,create_quiz
 
 
 print("...seeding database")   
-create_job_field()
-delete_job_field()
+# create_job_field()
+# delete_job_field()
+create_quiz()
