b219fc0d1859006c9b6fe9a9b0b27148d855a819
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-09-17 16:07:46 +0300

    creates an exit from app function

---
+++ lib/helpers.py
@@ -0,0 +1,46 @@
+from db.models import User, myengine
+from sqlalchemy.orm import sessionmaker
+
+session1 = sessionmaker(bind=myengine)
+mysession = session1()
+
+def exit_program():
+  print('Good Luck in your Interview')
+  exit()
+
+def login():
+  print("welcome back")
+
+# Class USER 
+#def create user()
+
+#def delete user()
+#def list_users()
+
+#Class JOB_FIELD
+#def create job_field()
+#def delete job_field()
+#def list_jobs_fields()
+#def find_by_id()
+
+#Class Topic
+#def create topic
+#def delete topic
+#def list_topics()
+#def find_by_id
+
+#Class Question
+#def create questions
+#def delete question
+#def list_questions()
+#def find_by_id
+
+#Class Answer
+#def create answers
+#def delete answers
+#def list_answers()
+#def find_by_id
+
+#def list_topic_questions()
+#def list_question_answers()
+#def list_job_field_topics()
