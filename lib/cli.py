78a427aa782f1f28c10a9acc32b1d1e2b486c3b3
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-09-17 19:01:45 +0300

    adds menu 2 to handle logged in user options

--- lib/cli.py
+++ lib/cli.py
@@ -1,7 +1,7 @@
 from helpers import (
-  exit_program,
-  create_user,
-  login
+  exit_program, create_user, login,
+  take_quiz, create_quiz,
+  delete_user,
 )
 
 def app():
@@ -11,15 +11,39 @@ def app():
     if choice == "0":
        exit_program()
     elif choice == "1":
-       create_user()  
+       create_user()
+       menu2()
+         
     elif choice == "2":
-       login()    
+       login()   
+       menu2() 
+    elif choice == "5":
+       delete_user()   
+       
 
 def menu():
-  print("Please select an option: ") 
+  print("Welcome to Interview Master. How can we help you today? ") 
   print("0. Exit")
   print("1. Create account")
   print("2. Login")
+  print("5. Delete my account")
+
+def menu2():
+   while True:
+       print("3. Take A quiz")
+       print("4. Create a quiz")
+       print("5. Delete my account")
+       print("0. Logout")
+       
+       choice = input("> ")
+       if choice == "3":
+          take_quiz()
+       elif choice == "4":
+          create_quiz()
+       elif choice == "5":
+          delete_user()
+       elif choice == "0":
+          exit_program()      
 
 if __name__ == "__main__":
      app()     
