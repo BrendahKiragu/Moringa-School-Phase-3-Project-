f31704e5294ad510e94355ff7f807b76fe90b052
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-09-18 22:55:11 +0300

    adds users and job fields functions

--- lib/debug.py
+++ lib/debug.py
@@ -1,6 +1,14 @@
-from helpers import(list_questions, find_question_by_id, list_topics, find_topic_by_name)
+from helpers import(
+  list_questions, find_question_by_id, list_topics, find_topic_by_name,
+  list_job_fields, find_field_by_name, list_users, find_user_by_name, show_jobs, show_field_topics)
 
 # list_questions()
 # find_question_by_id()
 # list_topics()
 # find_topic_by_name()
+# list_job_fields()
+# find_field_by_name()
+# list_users()
+# find_user_by_name()
+# show_jobs()
+show_field_topics()
