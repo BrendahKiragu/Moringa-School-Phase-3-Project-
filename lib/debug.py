7b21b9b3b86ce3b28efd43ceca888839231acd0c
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-09-19 11:47:39 +0300

    updated

--- lib/debug.py
+++ lib/debug.py
@@ -1,14 +1,13 @@
+#!/usr/bin/env python3
 from helpers import(
-  list_questions, find_question_by_id, list_topics, find_topic_by_name,
-  list_job_fields, find_field_by_name, list_users, find_user_by_name, show_jobs, show_field_topics)
+  list_questions, find_question_by_id, list_topics,
+  list_job_fields, find_field_by_name, list_users, find_user_by_name, show_jobs)
 
 # list_questions()
 # find_question_by_id()
 # list_topics()
-# find_topic_by_name()
 # list_job_fields()
 # find_field_by_name()
 # list_users()
 # find_user_by_name()
 # show_jobs()
-show_field_topics()
